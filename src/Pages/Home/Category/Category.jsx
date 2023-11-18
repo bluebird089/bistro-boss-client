c9fbba3a80efa5a2ea43fb80486eecae2684ca13
Blue Bird <bluebird089@proton.me>
2023-11-18 18:58:30 +0600

    added dynamic section heading

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -7,50 +7,57 @@ import Slide2 from "../../../assets/home/slide2.jpg";
 import Slide3 from "../../../assets/home/slide3.jpg";
 import Slide4 from "../../../assets/home/slide4.jpg";
 import Slide5 from "../../../assets/home/slide5.jpg";
+import SectionHeading from "../../../Components/SectionHeading/SectionHeading";
 
 const Category = () => {
     return (
-        <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            centeredSlides={true}
-            pagination={{
-                clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-        >
-            <SwiperSlide>
-                <img className="w-full" src={Slide1} alt="" />
-                <h3 className="text-4xl font-semibold text-white text-center -mt-20">
-                    Salad
-                </h3>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className="w-full" src={Slide2} alt="" />
-                <h3 className="text-4xl font-semibold text-white text-center -mt-20">
-                    Pizza
-                </h3>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className="w-full" src={Slide3} alt="" />
-                <h3 className="text-4xl font-semibold text-white text-center -mt-20">
-                    Soup
-                </h3>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className="w-full" src={Slide4} alt="" />
-                <h3 className="text-4xl font-semibold text-white text-center -mt-20">
-                    Desert
-                </h3>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className="w-full" src={Slide5} alt="" />
-                <h3 className="text-4xl font-semibold text-white text-center -mt-20">
-                    Salad
-                </h3>
-            </SwiperSlide>
-        </Swiper>
+        <div>
+            <SectionHeading
+                subHeading={"From 11 AM to !0 PM"}
+                heading={"Order Online"}
+            ></SectionHeading>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img className="w-full" src={Slide1} alt="" />
+                    <h3 className="text-4xl font-semibold text-white text-center -mt-20">
+                        Salad
+                    </h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className="w-full" src={Slide2} alt="" />
+                    <h3 className="text-4xl font-semibold text-white text-center -mt-20">
+                        Pizza
+                    </h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className="w-full" src={Slide3} alt="" />
+                    <h3 className="text-4xl font-semibold text-white text-center -mt-20">
+                        Soup
+                    </h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className="w-full" src={Slide4} alt="" />
+                    <h3 className="text-4xl font-semibold text-white text-center -mt-20">
+                        Desert
+                    </h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className="w-full" src={Slide5} alt="" />
+                    <h3 className="text-4xl font-semibold text-white text-center -mt-20">
+                        Salad
+                    </h3>
+                </SwiperSlide>
+            </Swiper>
+        </div>
     );
 };
 
