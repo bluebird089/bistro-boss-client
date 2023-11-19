7b58b4a11f300f56202aa6a544d797410176f42d
Blue Bird <bluebird089@proton.me>
2023-11-19 17:36:08 +0600

    added testimonial section to home page

---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,56 @@
+import SectionHeading from "../../../Components/SectionHeading/SectionHeading";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+
+const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("reviews.json")
+            .then((res) => res.json())
+            .then((data) => setReviews(data));
+    }, []);
+
+    return (
+        <div>
+            <SectionHeading
+                subHeading={"What Our Client Say About Us"}
+                heading={"Testimonials"}
+            ></SectionHeading>
+            <div>
+                <Swiper
+                    navigation={true}
+                    modules={[Navigation]}
+                    className="mySwiper"
+                >
+                    {reviews.map((review) => (
+                        <SwiperSlide key={review._id}>
+                            <div className="flex flex-col items-center text-center gap-3 m-10 p-10">
+                                <Rating
+                                    style={{ maxWidth: 180 }}
+                                    value={review.rating}
+                                    readOnly
+                                />
+                                <h3>{review.details}</h3>
+                                <p className="font-semibold text-yellow-600">
+                                    {review.name}
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
