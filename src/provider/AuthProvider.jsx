810ece87d7b9e22b61de851b82c20b27e92eead0
Blue Bird <bluebird089@proton.me>
2023-11-27 01:00:10 +0600

    added code to create token in local storage while login and delete while log out

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -54,16 +54,14 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
             if (currentUser) {
-                const userInfo = {
-                    email: currentUser.email,
-                };
+                const userInfo = { email: currentUser.email };
                 axiosPublic.post("/jwt", userInfo).then((res) => {
                     if (res.data.token) {
-                        localStorage.setItem("access token", res.data.token);
+                        localStorage.setItem("access-token", res.data.token);
                     }
                 });
             } else {
-                localStorage.removeItem("access token");
+                localStorage.removeItem("access-token");
             }
             setLoading(false);
         });
