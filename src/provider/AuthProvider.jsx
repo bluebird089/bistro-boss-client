d423d07635d501a486ba1fc7db926ae060f0d239
Blue Bird <bluebird089@proton.me>
2023-11-27 00:13:28 +0600

    reverted some changes

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
     updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
@@ -19,6 +20,8 @@ export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -50,13 +53,24 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
-            console.log(currentUser);
+            if (currentUser) {
+                const userInfo = {
+                    email: currentUser.email,
+                };
+                axiosPublic.post("/jwt", userInfo).then((res) => {
+                    if (res.data.token) {
+                        localStorage.setItem("access token", res.data.token);
+                    }
+                });
+            } else {
+                localStorage.removeItem("access token");
+            }
             setLoading(false);
         });
         return () => {
             return unsubscribe;
         };
-    }, []);
+    }, [axiosPublic]);
 
     const authInfo = {
         user,
