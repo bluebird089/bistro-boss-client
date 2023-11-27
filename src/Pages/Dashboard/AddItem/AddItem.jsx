a1eb4f8eb70eb64f8ac008fb14ee5b8f0e13f659
Blue Bird <bluebird089@proton.me>
2023-11-28 03:06:55 +0600

    addded add item page

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,10 +1,46 @@
 import { useForm } from "react-hook-form";
 import SectionHeading from "../../../Components/SectionHeading/SectionHeading";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+const imageHostingKey = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const imageHostingApi = `https://api.imgbb.com/1/upload?key=${imageHostingKey}`;
 
 const AddItem = () => {
-   const { register, handleSubmit } = useForm();
-   const onSubmit = (data) => console.log(data);
+   const { register, handleSubmit, reset } = useForm();
+   const axiosPublic = useAxiosPublic();
+   const axiosSecure = useAxiosSecure();
+   const onSubmit = async (data) => {
+      console.log(data);
+      const imageFile = { image: data.image[0] };
+      const res = await axiosPublic.post(imageHostingApi, imageFile, {
+         headers: {
+            "content-type": "multipart/form-data",
+         },
+      });
+      if (res.data.success) {
+         const menuItem = {
+            name: data.name,
+            category: data.category,
+            price: parseFloat(data.price),
+            recipe: data.recipe,
+            image: res.data.data.display_url,
+         };
+         const menuRes = await axiosSecure.post("/menu", menuItem);
+         console.log(menuRes.data);
+         if (menuRes.data.insertedId) {
+            reset()
+            Swal.fire({
+               title: "Success",
+               text: "Added Food Successfully",
+               icon: "success"
+             });
+         }
+      }
+      console.log(res.data);
+   };
    return (
       <div>
          <SectionHeading
@@ -70,9 +106,15 @@ const AddItem = () => {
                   />
                </div>
                <div className="form-control w-full mt-5">
-                  <input type="file" {...register("image")} className="file-input w-full" />
+                  <input
+                     type="file"
+                     {...register("image")}
+                     className="file-input w-full"
+                  />
                </div>
-               <button className="btn mt-5 w-full">Add Item <FaUtensils></FaUtensils> </button>
+               <button className="btn mt-5 w-full">
+                  Add Item <FaUtensils></FaUtensils>{" "}
+               </button>
             </form>
          </div>
       </div>
